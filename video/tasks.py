7c77a528126752d442795028048bf375f92f11c0
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2023-11-10 02:11:08 +0530

    updated app datastorage

--- video/tasks.py
+++ video/tasks.py
@@ -3,6 +3,8 @@ import subprocess
 from .models import VideoUpload, Subtitles
 import os
 from django.conf import settings
+import boto3
+import requests
 
 def parse_time_range(time_range):
     start, end = time_range.split(' --> ')
@@ -10,8 +12,16 @@ def parse_time_range(time_range):
 
 @shared_task
 def process_video(video_id):
-    video = VideoUpload.objects.get(id=video_id)
-    input_path = video.file.path
+    video = VideoUpload.objects.get(uuid=video_id)
+    # video_url = video.file.url
+    # print(video_url)
+    # s3 = boto3.client('s3',region_name='ap-south-1')
+    input_path = os.path.join(settings.MEDIA_ROOT,"uploads",f'video_{video_id}.mp4')
+    # s3.download_file('nvsvenakt', video_url, input_path)
+    # response = requests.get(video_url, stream=True)
+    # with open(input_path, 'wb') as file:
+    #     for chunk in response.iter_content(chunk_size=128):
+    #         file.write(chunk)
     output_path = os.path.join(settings.MEDIA_ROOT,"subtitles",f'subtitles_{video_id}.srt')
 
     ccextractor_command = f'ccextractorwinfull {input_path} -o {output_path}'
@@ -20,7 +30,6 @@ def process_video(video_id):
     with open(output_path, 'r') as subtitle_file:
         subtitle_text = subtitle_file.read()
         subtitle_entries = subtitle_text.split('\n\n')
-        j=0
         for i in subtitle_entries:
             lines = i.split('\n')
             if len(lines) >= 3:
@@ -28,7 +37,20 @@ def process_video(video_id):
                 time_range = lines[1]
                 start_time, end_time = parse_time_range(time_range)
                 content = '\n'.join(lines[2:])
-                subtitle = Subtitles(video=video, start_time=start_time, end_time=end_time, subtitle_text=content)
-                subtitle.save()
+                content = content.strip()
+                store_subtitle_in_dynamodb(video_id, start_time, content,end_time)
+                # subtitle = Subtitles(video=video, start_time=start_time, end_time=end_time, subtitle_text=content)
+                # subtitle.save()
 
     return "done"
+
+def store_subtitle_in_dynamodb(video_id, timestamp, subtitle,end_time):
+    dynamodb = boto3.client('dynamodb',region_name='ap-south-1')
+    table_name = 'nvstable_nosql'
+    item = {
+        'video':{'S': end_time},
+        'VideoId': {'S': str(video_id)},
+        'Timestamp': {'S': timestamp},
+        'Subtitle': {'S': subtitle},
+    }
+    dynamodb.put_item(TableName=table_name, Item=item)
--- video/views.py
+++ video/views.py
@@ -3,6 +3,10 @@ from .forms import VideoUploadForm
 from .tasks import process_video
 from .models import Subtitles,VideoUpload
 from django.http import HttpResponse
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 def video_list(request):
     videos = VideoUpload.objects.all()
@@ -13,7 +17,11 @@ def upload_video(request):
         form = VideoUploadForm(request.POST, request.FILES)
         if form.is_valid():
             video = form.save()
-            process_video.delay(video.id)
+            video_file = request.FILES['file']
+            fs = FileSystemStorage()
+            filename = fs.save(f'uploads/video_{video.uuid}.mp4', video_file)
+            process_video.delay(video.uuid)
+            messages.info(request,"The video is processing")
             return redirect('video_list')
     else:
         form = VideoUploadForm()
@@ -44,3 +52,37 @@ def play_video(request, video_id):
     with open(video_path, 'rb') as video_file:
         response = HttpResponse(video_file.read(), content_type='video/mp4')
         return response
+    
+
+def search_videos2(request, video_id):
+    keyword = request.GET.get('keyword')
+    results = []
+    context = None
+
+    if keyword:
+        dynamodb = boto3.client('dynamodb',region_name='ap-south-1')
+        table_name = 'nvstable_nosql'
+        expression_attribute_values = {
+            ':keyword': {'S': keyword},
+            ':video': {'S': video_id}
+        }
+        filter_expression = 'VideoId = :video AND contains(Subtitle, :keyword)'
+        response = dynamodb.scan(
+            TableName=table_name,
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=expression_attribute_values
+        )
+        # results = response.get('Items', [])
+        # filter_expression = Attr('video').contains(keyword) & Attr('VideoId').eq(str(video_id))
+
+        # # Perform the scan operation
+        # response = dynamodb.scan(
+        #     TableName=table_name,
+        #     FilterExpression=str(filter_expression)
+        # )
+
+        # Extract the items from the response
+        results = response.get('Items', [])
+        print(results)
+        context = {'results': results, 'keyword': keyword}
+    return render(request, 'search2.html', context)
